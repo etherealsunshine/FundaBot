f128ee02627089ea636056a68e75222b98bd56db
Aarav Navani <aarav.navani@outlook.com>
2021-04-22 09:19:54 +0530

    refactored a lot

--- main.py
+++ main.py
@@ -1,46 +1,30 @@
 import discord
 from discord.ext import commands
-from googletrans import Translator
-import random
 import json
 
-from cogs.translate import Translate
+from cogs import translate, misc
 
-client = commands.Bot(command_prefix=commands.when_mentioned_or("?"), help_command=None)
-translator = Translator()
+description = '''
+A bot dedicated to helping the members of the fundamics community
+'''
 
 with open("config.json", "r") as file:
     config = json.load(file)
 
-client.add_cog(Translate(client))
+client = commands.Bot(command_prefix=commands.when_mentioned_or("-"), description=description)
 
+client.add_cog(translate.Translate(client))
+client.add_cog(misc.Miscellaneous(client))
 
+# on ready event, set status here
 @client.event
 async def on_ready():
-    game = discord.Game("?help")
+    game = discord.Game("-help")
     await client.change_presence(status=discord.Status.idle, activity=game)
 
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(client.user.name + client.user.discriminator)
     print('------')
-
-
-@client.command(name='choice')
-async def random_choice(ctx, *, arg):
-    options = arg.split('|')
-
-    if len(options) < 2:
-        await ctx.send("need more options")
-        return
-
-    await ctx.send(random.choice(options))
-
-
-@random_choice.error
-async def random_choice_error(ctx, error):
-    if isinstance(error, commands.errors.MissingRequiredArgument):
-        await ctx.send("need more options")
 
 
 client.run(config["token"])
