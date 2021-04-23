2e721b95554ff0c4be19885a49d2a74a6c4319ef
Aarav Navani <aarav.navani@outlook.com>
2021-04-23 19:19:54 +0530

    refactored to check for members

--- main.py
+++ main.py
@@ -1,29 +1,39 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 import json
 
 from cogs import translate, misc
 
 description = '''
-A bot dedicated to helping the members of the fundamics community
+A bot dedicated to helping the members of the Fundamics community
 '''
+
+intents = discord.Intents.default()
+intents.members = True
 
 with open("config.json", "r") as file:
     config = json.load(file)
 
-client = commands.Bot(command_prefix=commands.when_mentioned_or("-"), description=description)
+client = commands.Bot(command_prefix=commands.when_mentioned_or("-"), description=description,intents=intents)
 
 client.add_cog(translate.Translate(client))
 client.add_cog(misc.Miscellaneous(client))
 
-# on ready event, set status here
+@tasks.loop(minutes=1)
+async def get_member_count():
+    fundamics = client.get_guild(config["serverid"])
+    count = len([s for s in fundamics.members if s.bot != True])
+    
+    game = discord.Game(f"-help | Watching {count} members")
+    await client.change_presence(status=discord.Status.idle, activity=game)
+    
+
 @client.event
 async def on_ready():
-    game = discord.Game("-help")
-    await client.change_presence(status=discord.Status.idle, activity=game)
+    get_member_count.start()
 
     print('Logged in as')
-    print(client.user.name + client.user.discriminator)
+    print(client.user.name + "#" + client.user.discriminator)
     print('------')
 
 
