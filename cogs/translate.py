5ffa0fa82a4447956a2110d91a271cd3f831bd2f
Aarav Navani <aarav.navani@outlook.com>
2021-05-02 15:55:59 +0530

    Added language based translation

--- cogs/translate.py
+++ cogs/translate.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from googletrans import Translator
+from utils import misc, formatter
 
 
 class Translate(commands.Cog, name='Translate'):
@@ -10,9 +11,16 @@ class Translate(commands.Cog, name='Translate'):
         self._translator = Translator()
 
     @commands.command(name='translate')
-    async def translate(self, ctx, *, text):
+    async def translate(self, ctx, language, *, text):
         """Translate text into english"""
-        output = self._translator.translate(text)
+        check_language = language.lower()
+        
+        if not misc.check_language(check_language):
+            return await ctx.send(f"Sorry, but {language} doesn't seem to be a valid language for translation, try `-languages` to get a list of translation languages")
+        
+        dest = misc.get_language_code(check_language)
+        
+        output = self._translator.translate(text, dest=dest)
         embed = discord.Embed(title='Translated', color=0xBFEDDD)
         embed.add_field(name=f'From {output.src}', value=output.origin, inline=False)
         embed.add_field(name=f'To {output.dest}', value=output.text, inline=False)
@@ -21,7 +29,14 @@ class Translate(commands.Cog, name='Translate'):
     @translate.error
     async def translate_error(self, ctx, error):
         if isinstance(error, commands.errors.MissingRequiredArgument):
-            await ctx.send("need something to translate")  
+            await ctx.send(f"missing an arguement, {error.param}")  
+            
+    @commands.command(name='languages')
+    async def languages(self, ctx):
+        langs = misc.get_languages()
+        lang_output = formatter.quote_list(langs)
+        embed = discord.Embed(title='Translation Languages', color=0xBFEDDD, description=lang_output)
+        await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Translate(bot))
