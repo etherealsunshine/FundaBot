84ae553a43db8c6388aeb11a3d141cd3f1666b7a
Aarav Navani <aarav.navani@outlook.com>
2021-05-02 12:04:42 +0530

    Changes

--- cogs/translate.py
+++ cogs/translate.py
@@ -11,17 +11,17 @@ class Translate(commands.Cog, name='Translate'):
 
     @commands.command(name='translate')
     async def translate(self, ctx, *, text):
-        """Translate a given piece of text to English"""
+        """Translate text into english"""
         output = self._translator.translate(text)
         embed = discord.Embed(title='Translated', color=0xBFEDDD)
         embed.add_field(name=f'From {output.src}', value=output.origin, inline=False)
         embed.add_field(name=f'To {output.dest}', value=output.text, inline=False)
         await ctx.send(embed=embed)
-
+ 
     @translate.error
     async def translate_error(self, ctx, error):
         if isinstance(error, commands.errors.MissingRequiredArgument):
-            await ctx.send("need something to translate")
+            await ctx.send("need something to translate")  
 
 def setup(bot):
     bot.add_cog(Translate(bot))
