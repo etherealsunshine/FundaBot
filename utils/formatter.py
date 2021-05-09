4c0b45ecf4d74c64fb76834cd42b3169f8d834c1
Aarav Navani <aarav.navani@outlook.com>
2021-05-09 21:31:36 +0530

    Added mark down formatter

--- utils/formatter.py
+++ utils/formatter.py
@@ -1,3 +1,6 @@
 def quote_list(items):
     output_items = [str(x) for x in items]
     return "> {items}".format(items ="\n> ".join(output_items))
+
+def mark_down_link(title, url):
+    return "[{title}](url)".format(title=title, url=url)
