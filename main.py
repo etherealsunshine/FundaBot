0ee1f5c41bfbc678b9847eacd78c0f191df717c2
Aarav Navani <aarav.navani@outlook.com>
2021-05-09 11:59:13 +0530

    Created a custom help command

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from utils import chat_utils
 # top level global variables
 fundachannel_id = 834066741076295750
 blackrose_id = 840618751648595978
+embed_color = 0xBFEDDD
 
 description = '''
 A bot dedicated to helping the members of the Fundamics community
@@ -47,15 +48,57 @@ list_trainer.train(chat_utils.movie_data)
 list_trainer.train(chat_utils.thing_data)
 
 
+async def get_response(input):
+    return chatbot.get_response(input)
+
 with open("config.json", "r") as file:
     config = json.load(file)
 
 
 client = commands.Bot(command_prefix="-", description=description,intents=intents)
 
+class FundaHelp(commands.HelpCommand):
+    def get_command_signature(self, command):
+        return '%s%s %s' % (self.clean_prefix, command.qualified_name, command.signature)
+    
+    async def send_error_message(self, error):
+        embed = discord.Embed(title="Error", description=error,color=embed_color)
+        channel = self.get_destination()
+        await channel.send(embed=embed)
 
-async def get_response(input):
-    return chatbot.get_response(input)
+    async def send_bot_help(self, mapping):
+        embed = discord.Embed(title="Help",color=embed_color)
+        for cog, commands in mapping.items():
+           filtered = await self.filter_commands(commands, sort=True)
+           command_signatures = [self.get_command_signature(c) for c in filtered]
+           if command_signatures:
+                cog_name = getattr(cog, "qualified_name", "No Category")
+                embed.add_field(name=cog_name, value="\n".join(command_signatures), inline=False)
+
+        channel = self.get_destination()
+        await channel.send(embed=embed)
+        
+    async def send_command_help(self, command):
+        embed = discord.Embed(title=self.get_command_signature(command),color=embed_color)
+        embed.add_field(name="Help", value=command.help)
+        alias = command.aliases
+        if alias:
+            embed.add_field(name="Aliases", value=", ".join(alias), inline=False)
+
+        channel = self.get_destination()
+        await channel.send(embed=embed)
+        
+    async def send_cog_help(self, cog):
+        embed = discord.Embed(title=cog.qualified_name,description=cog.description,color=embed_color)
+        commands = cog.get_commands()
+        filtered = await self.filter_commands(commands, sort=True)
+        embed.add_field(name='Commands', value="\n".join([c.name for c in filtered]))
+        
+        channel = self.get_destination()
+        await channel.send(embed=embed)
+        
+        
+client.help_command = FundaHelp()
 
 @client.listen('on_message')
 async def on_message(message):
