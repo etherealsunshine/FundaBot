f9d69c69b18f1e579ec686392a73062416de4837
Aarav Navani <aarav.navani@outlook.com>
2021-05-11 22:08:49 +0530

    modifications

--- utils/formatter.py
+++ utils/formatter.py
@@ -3,4 +3,4 @@ def quote_list(items):
     return "> {items}".format(items ="\n> ".join(output_items))
 
 def mark_down_link(title, url):
-    return "[{title}](url)".format(title=title, url=url)
+    return "[{title}]({url})".format(title=title, url=url)
